3abf4c4f8d9c4572f20e48edd4986055cc33f664
40314811 <bhall06@qub.ac.uk>
2022-10-26 20:30:40 +0100

    hold and wait for pair updated

--- index.php
+++ index.php
@@ -17,23 +17,26 @@ $mysqli = connect();
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
     $request_body = file_get_contents('php://input');
     $data = json_decode($request_body);
-    $js_data = array();
-    function remapJSobjectToPhpAssoc($data,$js_data) {
+    $request_from =  (empty($data)) ? "PHP":"JS";
+    if ($request_from == "JS") {
+        $js_data = array();
         foreach ($data as $k => $d) {
             $js_data[$k]=$d;
         }
-        return $js_data;
-    }
-    $js_data = @remapJSobjectToPhpAssoc($data,$js_data);
-    if (!empty($js_data)) {
         $_POST = $js_data;
+    }
+    if ($request_from == "PHP") {
+        // null
     }
 
     if (isset($_POST["shoplevel"])) {
         $shoplevel = $_POST["shoplevel"];
     }
     if (isset($_POST["team"])) {
-        $team  = $_POST["team"] ;
+        $team  = $_POST["team"];
+        if (isset($js_data)) {
+            $team = json_encode($team);
+        }
     }
     if (isset($_POST['id'])) {
         $team_id = $_POST['id'];
@@ -45,7 +48,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
         $does_it_exist = $mysqli->error;
         if ($does_it_exist) {
             // table doesn't exist, create then add user in
-            $create_table = "CREATE TABLE autopets$shoplevel (teamid int NOT NULL AUTO_INCREMENT, team varchar(255), paired varchar(255), PRIMARY KEY (teamid));";
+            $create_table = "CREATE TABLE autopets$shoplevel (teamid int NOT NULL AUTO_INCREMENT, team TEXT(4000), paired TEXT(2000), PRIMARY KEY (teamid));";
             $create_table_res = $mysqli->query($create_table);
             echo ($mysqli->error) ? $mysqli->error . $create_table : "";
             //add to table
@@ -55,17 +58,21 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
         $all = "SELECT * FROM autopets$shoplevel LIMIT 1";
         $all_res = $mysqli->query($all);
 
-        if ($all_res->num_rows>0) {
+        if ($all_res->num_rows>0 ) {
             // table does exist
             
             $opp = $all_res->fetch_assoc();
             $opp_team = $opp['team'];
             $opp_id = $opp['teamid'];
-            $add = "UPDATE autopets$shoplevel SET paired='$team' WHERE teamid = $opp_id";
-            $add_res = $mysqli->query($add);
-            echo ($mysqli->error) ? $mysqli->error . $add_res : "";
-            
-            echo json_encode(["opponent"=> $opp_team, "result"=> "paired"]);
+            if (!isset($team_id)) {
+
+                $add = "UPDATE autopets$shoplevel SET paired='$team' WHERE teamid = $opp_id";
+                $add_res = $mysqli->query($add);
+                echo ($mysqli->error) ? $mysqli->error . $add_res : "";
+                
+                echo json_encode(["opponent"=> $opp_team, "result"=> "paired"]);
+
+            }
             
         }
         if ($all_res->num_rows ==0) {
@@ -88,11 +95,14 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
         if ($all_res->num_rows!=0) {
             $data = $all_res->fetch_assoc();
             $opp_team = $data['paired'];
-            if ($opp_team !== NULL) {
+            if ($opp_team !== "0") {
 
                 $del = "DELETE FROM autopets$shoplevel  WHERE teamid = $team_id";
                 $del_res = $mysqli->query($del);
                 $data["message"] = "entry deleted";
+            } else {
+                $data["message"] = "still waiting for pairing";
+
             }
         
         }
